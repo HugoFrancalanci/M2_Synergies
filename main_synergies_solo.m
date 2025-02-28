00f7689490b7bd03eb39d75a77aaa63850360490
Hugo <francalanci.h@gmail.com>
2025-02-28 08:59:42 -0500

    2.solo)

--- main_synergies_solo.m
+++ main_synergies_solo.m
@@ -88,6 +88,96 @@ for subj_idx = 1:nb_subjects
     end
 end
 
+clc, clear all; 
+
+% Ajout de la bibliothèque btk
+addpath(genpath('C:\Users\Francalanci Hugo\Documents\MATLAB\Stage Sainte-Justine\HUG\btk'));
+
+% Définition des sujets
+subjects = {'AP06'};
+nb_subjects = length(subjects);
+
+% Définition des muscles 
+muscles = {'RDELTA', 'RDELTM', 'RDELTP', 'RTRAPM', 'RTRAPS', 'RLATD', 'RSERRA'};
+nb_muscles = length(muscles);
+
+% Définition des labels pour les tâches
+functional_labels = {'Functional Task 1', 'Functional Task 2', 'Functional Task 3', 'Functional Task 4'};
+analytic_labels = {'Analytic Task 1', 'Analytic Task 2', 'Analytic Task 3', 'Analytic Task 4'};
+
+nb_functional = length(functional_labels);
+nb_analytic = length(analytic_labels);
+
+% Sélection de l'analytique et du fonctionnel 
+selected_analytic = input('Choisissez une tâche analytique (1-4): ');
+selected_functional = input('Choisissez une tâche fonctionnelle (1-4): ');
+
+% Paramètres EMG
+fs = 2000;
+[b, a] = butter(4, [15, 475] / (fs/2), 'bandpass');
+rms_window = round(0.250 * fs);
+num_points = 1000;
+time_normalized = linspace(0, 1, num_points);
+
+% Initialisation des MVCs
+mvc_flexion = zeros(nb_muscles, 1);
+all_functional_data = cell(nb_subjects, nb_functional, nb_muscles);
+
+% Traitement de la tâche analytique choisie
+for subj_idx = 1:nb_subjects
+    fileName_analytic = sprintf(['C:\\Users\\Francalanci Hugo\\Documents\\MATLAB\\Stage Sainte-Justine\\HUG\\Sujets\\%s\\' ...
+        '%s-%s-20240101-PROTOCOL01-ANALYTIC%d-.c3d'], ...
+        subjects{subj_idx}, subjects{subj_idx}, subjects{subj_idx}, selected_analytic);
+    
+    c3dH_analytic = btkReadAcquisition(fileName_analytic);
+    analogs_analytic = btkGetAnalogs(c3dH_analytic);
+    
+    for m = 1:nb_muscles
+        muscle_name = muscles{m};
+        if isfield(analogs_analytic, muscle_name)
+            signal = analogs_analytic.(muscle_name);
+            signal_filtered = filtfilt(b, a, signal);
+            signal_abs = abs(signal_filtered);
+            emg_rms = sqrt(movmean(signal_abs.^2, rms_window));
+            mvc_flexion(m) = max(emg_rms);
+        end
+    end
+end
+mvc_max = max(mvc_flexion);
+
+% Traitement et normalisation de la tâche fonctionnelle choisie
+for subj_idx = 1:nb_subjects
+    fileName_functional = sprintf(['C:\\Users\\Francalanci Hugo\\Documents\\MATLAB\\Stage Sainte-Justine\\HUG\\Sujets\\%s\\' ...
+        '%s-%s-20240101-PROTOCOL01-FUNCTIONAL%d-.c3d'], ...
+        subjects{subj_idx}, subjects{subj_idx}, subjects{subj_idx}, selected_functional);
+    
+    c3dH_functional = btkReadAcquisition(fileName_functional);
+    analogs_functional = btkGetAnalogs(c3dH_functional);
+    
+    figure;
+    sgtitle(sprintf('Sujet %s - %s', subjects{subj_idx}, functional_labels{selected_functional}));
+    
+    for m = 1:nb_muscles
+        subplot(ceil(nb_muscles/2), 2, m);
+        muscle_name = muscles{m};
+        if isfield(analogs_functional, muscle_name)
+            signal = analogs_functional.(muscle_name);
+            signal_filtered = filtfilt(b, a, signal);
+            signal_abs = abs(signal_filtered);
+            emg_rms = sqrt(movmean(signal_abs.^2, rms_window));
+            emg_normalized = (emg_rms / mvc_max) * 100;
+            interp_signal_functional = interp1(linspace(0, 1, length(signal)), emg_normalized, time_normalized, 'spline');
+            all_functional_data{subj_idx, selected_functional, m} = interp_signal_functional;
+            plot(time_normalized, interp_signal_functional, 'LineWidth', 1.2);
+            title(muscle_name);
+            ylabel('EMG (% max flexion)');
+            grid on;
+        else
+            title(sprintf('%s (Données absentes)', muscle_name));
+        end
+    end
+end
+
 %% Calcul du VAF et détermination du nombre optimal de synergies 
 subject_idx = 1;
 functional_idx = selected_functional;
@@ -158,8 +248,7 @@ xlabel('Nombre de synergies');
 ylabel('VAF (%)');
 title('Courbe de VAF musculaire pour chaque muscle');
 grid on;
-legend(muscle_labels, 'Location', 'best'); % Ajouter une légende avec les noms des muscles
-
+legend(muscles, 'Location', 'best'); % Ajouter une légende avec les noms des muscles
 
 %% Visualisation des synergies musculaires extraites
 % Recalcul de la décomposition NNMF avec le nombre optimal de synergies
